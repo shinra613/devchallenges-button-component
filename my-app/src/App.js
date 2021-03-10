fc62ed7730a858bb73c0633bff0876b3311af75b
AkhilVenkateshPillai <akhiltechwiz@gmail.com>
2021-03-10 23:54:24 +0530

    -feat
    -added button module and related component.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,49 @@
-
+import React, { useState } from 'react';
 import './App.css';
 import Button from './components/button/Button'
 
 function App() {
+
+  const [module, setModule] = useState('Button');
+  
+  const moduleComponent = {
+    'Button': <Button />
+  }
+
+  const ModuleReturn = (moduleName) => {
+    
+    return moduleComponent[moduleName];
+    
+   }
+
+  const moduleFinder = (moduleName) => {
+   
+    setModule(moduleName);
+  }
+
+
+
   return (
     <div className="App">
       <div className="main-container">
         <div className="module">
         <h1 className="logoname"><span>Dev</span>challenges.io</h1>
           <ul>
-            <li>Colors</li>
-            <li>Typography</li>
-            <li>Spaces</li>
-            <li>Button</li>
-            <li>Input</li>
-            <li>Grid</li>
+            <li onClick={()=>moduleFinder('Colors')} >Colors</li>
+            <li onClick={()=>moduleFinder('Typography')} >Typography</li>
+            <li onClick={()=>moduleFinder('Spaces')} >Spaces</li>
+            <li onClick={()=>moduleFinder('Button')} >Button</li>
+            <li onClick={()=>moduleFinder('Input')} >Input</li>
+            <li onClick={()=>moduleFinder('Grid')}>Grid</li>
           </ul>
         </div>
         <div className="modulePage">
+          <div className="module-title">{module}</div>
+          <div>
+            
+            {ModuleReturn(module)}
+
+          </div>
           
         </div>
       </div>
--- my-app/src/components/button/Button.js
+++ my-app/src/components/button/Button.js
@@ -1,8 +1,39 @@
 import React from 'react';
 import './Button.css';
 
+
+
+function ButtonComp(props) {
+    return <button className={props.class}>Click Me</button>
+}
+
 function Button (){
-    return <button>Click Me</button>
+    return <div>
+
+        <table>
+            <tr>
+                <td><ButtonComp /></td>
+                <td><ButtonComp class="hoverOne" /></td>
+            </tr>
+            <tr>
+                <td><ButtonComp class="outline" /></td>
+                <td> <ButtonComp class="outline-hover"/></td>
+            </tr>
+            <tr>
+                <td><ButtonComp class="outline" /></td>
+                <td> <ButtonComp class="outline-hover"/></td>
+            </tr>
+            <tr>
+                <td><ButtonComp class="outline" /></td>
+                <td> <ButtonComp class="outline-hover"/></td>
+            </tr>
+        </table>
+
+       
+        
+        
+       
+    </div>
 }
 
 export default Button;
