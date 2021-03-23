9f2f49e91a708351ee0b1c74553c703ecf2476ac
AkhilVenkateshPillai <akhiltechwiz@gmail.com>
2021-03-24 00:18:11 +0530

    feat
    -included input module

--- my-app/src/components/input/input.js
+++ my-app/src/components/input/input.js
@@ -1,34 +1,64 @@
 import React from 'react';
-import './input';
+import './input.css';
 
+
+const InputComp = (props) => {
+    return (<div className="inpcon"><label>Label</label><input type="text" placeholder='placeholder'></input></div>)
+}
 
 const  Input=() => {
    
     return (<div>
         <table>
             <tr>
-                <td></td>
-                <td></td>
-                <td></td>
+                <td><InputComp /></td>
+                <td><InputComp /></td>
+                <td><InputComp /></td>
 
             </tr>
 
             <tr>
-            <td></td>
-            <td></td>
-            <td></td>
+            <td><InputComp /></td>
+            <td><InputComp /></td>
+            <td><InputComp /></td>
 
             </tr>
 
             <tr>
-            <td></td>
+            <td><InputComp /></td>
                 
+            </tr>
+        </table>
+        <table>
+            <tr>
+                <td><InputComp /></td>
+                <td><InputComp /></td>
+                
+
+            </tr>
+
+            <tr>
+            <td><InputComp /></td>
+            <td><InputComp /></td>
+            
+            </tr>
+
+            <tr>
+            <td><InputComp /></td>
+                
+            </tr>
+            <tr>
+            <td><InputComp /></td>
+            <td><InputComp /></td>
+            
             </tr>
         </table>
     </div>)
 
 
 }
+
+
 
 
 export default Input;
